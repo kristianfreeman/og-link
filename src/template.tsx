771348df0ba32f18d53a4c0ad39bdfa9fdfceaa6
Kristian Freeman <kristian@kristianfreeman.com>
2024-09-22 12:27:40 -0500

    2024-09-22-12:27:40

--- src/template.tsx
+++ src/template.tsx
@@ -1,53 +1,57 @@
 import React from 'react';
 
 export default (text: string, domain: string) => (
-  <div
-  style={{
-    width: '1200px',
-    height: '627px',
-    padding: '20px',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-    background: 'linear-gradient(120deg, #a1c4fd 0%, #c2e9fb 100%)',
-    color: '#333',
-    fontSize: '32px',
-    fontWeight: '600',
-    textAlign: 'center',
-    boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
-    borderRadius: '10px',
-    position: 'relative',
-  }}
->
-  <div
-    style={{
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-      flexGrow: 1, // Expand the container
-    }}
-  >
-    <h1
-      style={{
-        fontSize: '48px',
-        fontWeight: '700',
-        maxWidth: '90%',
-        wordWrap: 'break-word',
-      }}
-    >
-      {text}
-    </h1>
-  </div>
-  <p
-    style={{
-      fontSize: '24px',
-      color: '#666',
-      position: 'absolute',
-      bottom: '20px',
-    }}
-  >
-    {domain}
-  </p>
-</div>
+	<div
+		style={{
+			width: '1200px',
+			height: '627px',
+			padding: '20px',
+			display: 'flex',
+			flexDirection: 'column',
+			alignItems: 'center',
+			justifyContent: 'flex-start',
+			background: 'black',
+			color: 'white',
+			fontSize: '32px',
+			fontWeight: '600',
+			textAlign: 'center',
+			boxShadow: '0 0 10px rgba(255, 255, 255, 0.1)',
+			borderRadius: '10px',
+			position: 'relative',
+			fontFamily: 'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif',
+		}}
+	>
+		<div
+			style={{
+				display: 'flex',
+				flexDirection: 'column',
+				justifyContent: 'center',
+				flexGrow: 1,
+			}}
+		>
+			<h1
+				style={{
+					fontFamily: 'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif',
+					fontSize: '48px',
+					fontWeight: '700',
+					maxWidth: '90%',
+					wordWrap: 'break-word',
+				}}
+			>
+				{text}
+			</h1>
+		</div>
+		<p
+			style={{
+				fontFamily: 'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif',
+				fontSize: '24px',
+				color: '#ccc',
+				position: 'absolute',
+				bottom: '20px',
+			}}
+		>
+			{domain}
+		</p>
+	</div>
 );
+
--- src/worker.ts
+++ src/worker.ts
@@ -6,41 +6,41 @@ export interface Env {
 
 // see https://en.wikipedia.org/wiki/Base64#URL_applications
 function decodeBase64Url(base64Url: string) {
-  // Replace URL specific chars
-  base64Url = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+	// Replace URL specific chars
+	base64Url = base64Url.replace(/-/g, '+').replace(/_/g, '/');
 
-  // Add padding
-  while (base64Url.length % 4) {
-    base64Url += '=';
-  }
+	// Add padding
+	while (base64Url.length % 4) {
+		base64Url += '=';
+	}
 
-  const binStr = atob(base64Url);
-  const binStrLen = binStr.length;
-  const bytes = new Uint8Array(binStrLen);
+	const binStr = atob(base64Url);
+	const binStrLen = binStr.length;
+	const bytes = new Uint8Array(binStrLen);
 
-  for (let i = 0; i < binStrLen; i++) {
-    bytes[i] = binStr.charCodeAt(i);
-  }
+	for (let i = 0; i < binStrLen; i++) {
+		bytes[i] = binStr.charCodeAt(i);
+	}
 
-  return new TextDecoder().decode(bytes.buffer);
+	return new TextDecoder().decode(bytes.buffer);
 }
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-    // the path looks like /image/<payload>/og.png
-    const payload = request.url.split("/")[4];
+		// the path looks like /image/<payload>/og.png
+		const payload = request.url.split("/")[4];
 
-    if (!payload) {
-      return new Response("Missing payload", { status: 400 });
-    }
+		if (!payload) {
+			return new Response("Missing payload", { status: 400 });
+		}
 
-    const decodedBase64 = decodeBase64Url(payload);
-    const decoded = JSON.parse(decodedBase64);
-    const { title, domain = "www.skorfmann.com" } = decoded;
-    return new ImageResponse(template(title, domain), {
-      format: "png",
-      width: 1200,
-      height: 627,
-    });
-  }
+		const decodedBase64 = decodeBase64Url(payload);
+		const decoded = JSON.parse(decodedBase64);
+		const { title, domain = "kristianfreeman.com" } = decoded;
+		return new ImageResponse(template(title, domain), {
+			format: "png",
+			width: 1200,
+			height: 627,
+		});
+	}
 };
